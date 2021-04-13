e07f2bc1a0d301cd4e6fc6d7c0a9868d77f92206
brunocamposalves <brunocamposalves@hotmail.com>
2021-04-13 18:09:34 -0300

    feat: create react sctruture

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+// import { Container } from './styles';
+
+const Home = () => {
+  return (
+    <>
+      Home
+    </>
+  );
+};
+
+export default Home;
---
+++ src/pages/register/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Register = () => {
+  return <> Register </>;
+}
+
+export default Register;
