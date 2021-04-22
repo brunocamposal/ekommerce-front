a9173d9a4995267ef9f7996392fddfcdc7077ca3
eduardo <eduardo.magno.u@gmail.com>
2021-04-22 08:47:40 -0300

    remove product-modal from home

--- src/domains/home/pages/Home.tsx
+++ src/domains/home/pages/Home.tsx
@@ -1,35 +1,8 @@
-import React, { useState } from 'react';
-
-import { ProductModal } from '../../../shared/components/product-modal/ProductModal';
-
 export const HomePage = () => {
-  const [openModal, setOpenModal] = useState<boolean>(false);
-
-  const handleCloseModal = () => {
-    setOpenModal(false);
-  };
-
-  const handleOpenModal = () => {
-    setOpenModal(true);
-  };
-
-  const addToCart = () => {
-    console.log('add to card')
-  }
-
   return (
     <>
       <div>
-        <button onClick={handleOpenModal}>Abrir modal</button>
-        <ProductModal
-          open={openModal}
-          handleClose={handleCloseModal}
-          productTitle="Produto exemplo"
-          productDescription="DESCRIÇÃO DO PRODUTO BEM LONGA DESCRIÇÃO DO PRODUTO BEM LONGADESCRIÇÃO DO PRODUTO BEM LONGA DESCRIÇÃO DO PRODUTO BEM LONGADESCRIÇÃO DO PRODUTO BEM LONGADESCRIÇÃO DO PRODUTO BEM LONGADESCRIÇÃO DO PRODUTO BEM LONGADESCRIÇÃO DO PRODUTO BEM LONGADESCRIÇÃO DO PRODUTO BEM LONGAVDESCRIÇÃO DO PRODUTO BEM LONGA"
-          productImagePath="https://belezaesaude.com/i/730/56/tomate.jpg"
-          productPrice="2.30"
-          addToCart={addToCart}
-        ></ProductModal>
+        <h2> Home PAGEEEEE!!!</h2>
       </div>
     </>
   );
